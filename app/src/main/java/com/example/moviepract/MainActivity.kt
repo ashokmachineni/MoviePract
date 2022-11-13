45842b042d87fdc0baf2bcd38291fbd322456ad3
ashok <ashok@discoverbraid.com>
2022-11-13 20:11:59 +0000

    add card view

--- app/src/main/java/com/example/moviepract/MainActivity.kt
+++ app/src/main/java/com/example/moviepract/MainActivity.kt
@@ -3,18 +3,23 @@ package com.example.moviepract
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.moviepract.data.MovieItem
 import com.example.moviepract.presentation.MoviesViewModel
@@ -56,7 +61,15 @@ fun MovieScreen(
 
 @Composable
 fun MoviesListUI(movie: MovieItem){
-    Row(modifier = Modifier.fillMaxWidth()){
-        Text(text = movie.name)
-    }
+
+        Card(modifier = Modifier
+            .fillMaxWidth()
+            .shadow(elevation = 4.dp)
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()){
+                Text(text = movie.name)
+            }
+        }
+
+
 }
